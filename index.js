76178af016aa3722c2c2fddd995669a77908df06
Sonia <soniarr234@gmail.com>
2025-02-15 14:04:16 +0100

    Reemplazar bcrypt con bcryptjs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mysql = require('mysql2');
 require('dotenv').config();
 
